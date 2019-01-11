84436ae88cb9b19e487ff56601988770aebc6faf
sajibgoswami11 <sabujgoswami_cou@yahoo.com>
2019-01-11 18:38:38 +0600

    Appbar and grid

--- src/App/index.js
+++ src/App/index.js
@@ -2,22 +2,16 @@ import React, { Component } from 'react';
 import './App.css';
 import AppLayout from './AppLayout';
 import AppBar from './AppBar';
-import {AppProvider} from './AppProvider';
-import Settings from '../Settings';
-import Dashboard from '../Dashboard';
-import Content from '../Shared/Content';
+//import styled from 'styled-components';
+
+
 
 class App extends Component {
   render() {
     return (
-      <AppLayout>
-        <AppProvider>
-          <AppBar/>
-          <Content>
-            <Settings />
-            <Dashboard />
-          </Content>
-        </AppProvider>
+     <AppLayout>
+       <AppBar />
+	     HEllo
       </AppLayout>
     );
   }
